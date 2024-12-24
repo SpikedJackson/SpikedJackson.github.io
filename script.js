b8f61b4ccc24f5579c50357900878008defcde88
Jackson <43098221+SpikedJackson@users.noreply.github.com>
2024-12-24 16:23:30 -0500

    Enhanced About Me Section

--- script.js
+++ script.js
@@ -4,3 +4,49 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+ 
+consoleText();
+
+function consoleText() {
+    words = ['Developer', 'Student', 'Musician'];
+    var visible = true;
+    var con = document.getElementById('console');
+    var letterCount = 1;
+    var x = 1;
+    var waiting = false;
+    var target = document.getElementById('console-text');
+    target.setAttribute('style', 'color: #000000')
+    window.setInterval(function() {
+        if (letterCount === 0 && waiting === false) {
+            waiting = true;
+            target.innerHTML = words[0].substring(0, letterCount)
+            window.setTimeout(function() {
+                var usedWord = words.shift();
+                words.push(usedWord);
+                x = 1;
+                target.setAttribute('style', 'color: #000000')
+                letterCount += x;
+                waiting = false;
+            }, 1000)
+        } else if (letterCount === words[0].length + 1 && waiting === false) {
+            waiting = true;
+            window.setTimeout(function() {
+                x = -1;
+                letterCount += x;
+                waiting = false;
+            }, 1000)
+        } else if (waiting === false) {
+            target.innerHTML = words[0].substring(0, letterCount)
+            letterCount += x;
+        }
+    }, 120)
+    window.setInterval(function() {
+        if (visible === true) {
+            con.className = 'console-underscore hidden'
+            visible = false;
+        } else {
+            con.className = 'console-underscore'
+            visible = true;
+        }
+    }, 400)
+}
